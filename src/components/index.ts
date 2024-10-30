76db1d62928c63aac02dce9d10767a0873a8867f
hyperlolo <hyperlolo@gmail.com>
2024-10-29 23:37:44 -0500

    feat: added create note to home page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Button from "./button/button";
+import CreateNote from "./create-note/create-note";
 import Navbar from "./nav-bar/nav-bar";
 import NoteCard from "./note-card/note-card";
 
-export { Button, Navbar, NoteCard };
+export { Button, CreateNote, Navbar, NoteCard };
